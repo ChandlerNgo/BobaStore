78e1786498ed1190eda55fa2b97bd9495cb5ff08
Chandler Ngo <chandlerngo00@gmail.com>
2024-06-01 22:54:37 -0500

    products are now displayed in the menu

---
+++ frontend/src/components/ItemCard.js
@@ -0,0 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React,{useState} from 'react';
+
+export default function ItemCard(props){
+    const [items,setItems] = useState(Object.keys(props.menuItems[props.items])[0])
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        var itemName = props.items
+        var itemSize = items
+        var itemPrice = props.menuItems[props.items][items]
+        console.log(itemName + " " + itemSize + " " + itemPrice);
+    }
+
+    const handleChange = event => {
+        setItems(event.target.value);
+    };
+
+    return (
+        <Form className='row mx-auto' onSubmit={handleSubmit}>
+            <Form.Select aria-label="" className="col-md-6 w-50" value={items} onChange={handleChange}>
+                {Object.keys(props.menuItems[props.items]).map((size) => {
+                    return (
+                        <option key={props.items+size} value={size}>{size} | ${props.menuItems[props.items][size]}</option>
+                    )
+                })}
+            </Form.Select>
+            <Button className="col-md-6" variant="success" type="submit">Add to Cart</Button>
+        </Form>
+    )
+}
--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 import React,{useState, useEffect} from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
+import ItemCard from '../components/ItemCard';
 
 export default function Menu(props){
     const [menuItems,setMenuItems] = useState({})
@@ -18,39 +17,21 @@ export default function Menu(props){
         getMenuItems()
     }, []);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(event);
-    }
-
     return (
         <div className="row g-5 w-50 mx-auto">
-            Menu
-            {JSON.stringify(menuItems)}
+            {/* Menu */}
+            {/* {JSON.stringify(menuItems)} */}
             {Object.keys(menuItems).map((items) => {
                 return (
                     <div className="col-md-6 col-lg-4" key={items}>
                         <Card>
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                            {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
                             <Card.Body>
                                 <Card.Title>{items}</Card.Title>
                                 <Card.Text>
                                     button text
                                 </Card.Text>
-                                <div>
-                                    <Form className='row mx-auto' onSubmit={handleSubmit}>
-                                        <Form.Group>
-                                            <Form.Select aria-label="" className="col-md-6 w-50" id={items}>
-                                                {Object.keys(menuItems[items]).map((size) => {
-                                                    return (
-                                                        <option key={items+size} value={size}>{size} | ${menuItems[items][size]}</option>
-                                                    )
-                                                })}
-                                            </Form.Select>
-                                        </Form.Group>
-                                        <Button className="col-md-6" variant="success" type="submit"></Button>
-                                    </Form>
-                                </div>
+                                <ItemCard menuItems={menuItems} items={items}/>
                             </Card.Body>
                         </Card>
                     </div>
